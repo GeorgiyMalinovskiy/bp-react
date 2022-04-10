7b56a8da2ff04e434fe11337a086aba5ffe00520
Georgiy Malinovskiy <post@gmalinovskiy.su>
2022-04-10 22:36:45 +0300

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["plugin:react/recommended", "airbnb", "prettier", "plugin:jsx-a11y/recommended"],
+    plugins: ["react", "react-hooks", "import", "jest", "jsx-a11y", "@typescript-eslint", "prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    settings: {
+        "import/resolver": {
+            node: {
+                paths: ["src", "scripts"],
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+    },
+    rules: {
+        "react/jsx-uses-react": "off",
+        "react/react-in-jsx-scope": "off",
+        "react/jsx-filename-extension": [
+            1,
+            {
+                extensions: [".jsx", ".tsx"],
+            },
+        ],
+        "import/order": [
+            "error",
+            {
+                groups: ["builtin", "external", "internal"],
+                pathGroups: [
+                    {
+                        pattern: "react",
+                        group: "external",
+                        position: "before",
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ["react"],
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true,
+                },
+            },
+        ],
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                js: "never",
+                jsx: "never",
+                ts: "never",
+                tsx: "never",
+            },
+        ],
+        "prettier/prettier": 2,
+    },
+};
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const HtmlWeboackPlugin = require("html-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+
+console.log("Mode: ", process.env.NODE_ENV); // eslint-disable-line no-console
+module.exports = {
+    mode,
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"],
+                    plugins: ["@babel/plugin-transform-runtime"],
+                },
+                exclude: /node_modules/,
+            },
+            { test: /\.js$/, loader: "source-map-loader" },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+        new HtmlWeboackPlugin({
+            publicPath: "/",
+            template: path.resolve(__dirname, "src", "static", "index.html"),
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        historyApiFallback: true,
+        hot: true,
+        port: 3000,
+    },
+};
